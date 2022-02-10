c6244d8342984b341e32dcc258b8ef8049ac639e
linux_china <libing.chen@gmail.com>
2022-02-09 17:42:36 -0800

    Hello.java with spring native support

---
+++ demo/src/hello/Hello.java
@@ -0,0 +1,22 @@
+///usr/bin/env jbang "$0" "$@" ; exit $?
+//JAVA 11
+//DEPS org.mvnsearch:jbang-spring-native:2.6.3-SNAPSHOT
+
+package hello;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hello implements CommandLineRunner {
+
+    public static void main(String... args) {
+        SpringApplication.run(Hello.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Hello Spring Native!");
+    }
+}
