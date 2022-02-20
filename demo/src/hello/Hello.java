938b068e6045550281bfb485f168096633b39325
linux_china <libing.chen@gmail.com>
2022-02-19 20:12:15 -0800

    Add native-image-agent

--- demo/src/hello/Hello.java
+++ demo/src/hello/Hello.java
@@ -1,6 +1,7 @@
 ///usr/bin/env jbang "$0" "$@" ; exit $?
 //JAVA 11+
 //DEPS org.mvnsearch:jbang-spring-native:2.6.3-1
+//JAVA_OPTIONS -agentlib:native-image-agent=config-merge-dir=/tmp/native-image-agent
 
 package hello;
 
