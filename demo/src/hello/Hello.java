3beb2111dd19b86600bad5a3efb1392197eed9d0
linux_china <libing.chen@gmail.com>
2022-04-06 09:54:39 -0700

    Update to org.mvnsearch:jbang-spring-native:2.6.6

--- demo/src/hello/Hello.java
+++ demo/src/hello/Hello.java
@@ -1,6 +1,6 @@
 ///usr/bin/env jbang "$0" "$@" ; exit $?
 //JAVA 11+
-//DEPS org.mvnsearch:jbang-spring-native:2.6.4
+//DEPS org.mvnsearch:jbang-spring-native:2.6.6
 //JAVA_OPTIONS -agentlib:native-image-agent=config-merge-dir=/tmp/native-image-agent
 
 package hello;
@@ -18,6 +18,6 @@ public class Hello implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        System.out.println("Hello JBang with Spring Native");
+        System.out.println("Hello JBang with Spring Native!");
     }
 }
