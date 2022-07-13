7702109d75fd505d698d083ffd124648c6e09bb8
linux_china <libing.chen@gmail.com>
2022-07-13 09:11:44 -0700

    chore: update to jbang-spring-native:2.7.1

--- demo/src/hello/Hello.java
+++ demo/src/hello/Hello.java
@@ -1,6 +1,6 @@
 ///usr/bin/env jbang "$0" "$@" ; exit $?
 //JAVA 11+
-//DEPS org.mvnsearch:jbang-spring-native:2.7.0
+//DEPS org.mvnsearch:jbang-spring-native:2.7.1
 //JAVA_OPTIONS -agentlib:native-image-agent=config-merge-dir=/tmp/native-image-agent
 
 package hello;
